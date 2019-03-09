0b0580cb14b09d2d3cf901f15e270405403a15ac
Adam Selker <adamselker@gmail.com>
2019-03-09 14:59:49 -0500

    capitalize mA right

--- exp2.py
+++ exp2.py
@@ -42,8 +42,8 @@ ax = plt.subplot(111)
 
 # Sink log-log plot
 for i in range(3):
-  ax.loglog(ix1[i], iz1[i], ['r.', 'g.', 'b.'][i], label="Iz (Iy = %s ma)" % iy_names[i])
-  ax.loglog(ix1[i], iz1t[i], ['r-', 'g-', 'b-'][i], label="Theoretical fit (Iy = %s ma)" % iy_names[i])
+  ax.loglog(ix1[i], iz1[i], ['r.', 'g.', 'b.'][i], label="Iz (Iy = %s mA)" % iy_names[i])
+  ax.loglog(ix1[i], iz1t[i], ['r-', 'g-', 'b-'][i], label="Theoretical fit (Iy = %s mA)" % iy_names[i])
 
 plt.title("Geometric mean finder with fixed Iy")
 plt.xlabel("Input current Ix (A)")
@@ -55,8 +55,8 @@ ax.cla()
 
 # Source log-log plot
 for i in range(3):
-  ax.loglog(iy2[i], iz2[i], ['r.', 'g.', 'b.'][i], label="Iz (Ix = %s ma)" % ix_names[i])
-  ax.loglog(iy2[i], iz2t[i], ['r-', 'g-', 'b-'][i], label="Theoretical fit (Ix = %s ma)" % ix_names[i])
+  ax.loglog(iy2[i], iz2[i], ['r.', 'g.', 'b.'][i], label="Iz (Ix = %s mA)" % ix_names[i])
+  ax.loglog(iy2[i], iz2t[i], ['r-', 'g-', 'b-'][i], label="Theoretical fit (Ix = %s mA)" % ix_names[i])
 
 plt.title("Geometric mean finder with fixed Ix")
 plt.xlabel("Input current Iy (A)")
