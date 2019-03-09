1acfb97705cb199495dd81020d928d164d0805cc
Adam Selker <adamselker@gmail.com>
2019-03-08 22:57:55 -0500

    better fits

--- exp1.py
+++ exp1.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 vrangeAll = (0, 1) # Throw away data points outside this voltage range
-vrangeFit = (0.4, 0.625) # Use data points inside this range for curve-fitting
 
 vb_exp = [[],[],[],[]]
 ib_exp = [[],[],[],[]]
@@ -36,6 +35,7 @@ Iss = [0,0,0,0]
 for i in range(4):
 
   # First, filter the data so we only fit to the right parts
+  vrangeFit = (0.2, 0.65) # Use data points inside this range for curve-fitting
   pts = zip(vb_exp[i], ib_exp[i], ic_exp[i])
   validPts = [pt for pt in pts if (vrangeFit[0] <= pt[0]) and (pt[0] <= vrangeFit[1]) and 0 < pt[1] and 0 < pt[2]]
   thisVb, thisIb, thisIc = list(zip(*validPts))
@@ -45,6 +45,12 @@ for i in range(4):
 
   def ib_f(Vbe, β):
     return (Is/β) * (np.exp(Vbe/Ut) - 1)
+
+  # Re-filter the data
+  vrangeFit = (0.575, 0.625) # Use data points inside this range for curve-fitting
+  pts = zip(vb_exp[i], ib_exp[i], ic_exp[i])
+  validPts = [pt for pt in pts if (vrangeFit[0] <= pt[0]) and (pt[0] <= vrangeFit[1]) and 0 < pt[1] and 0 < pt[2]]
+  thisVb, thisIb, thisIc = list(zip(*validPts))
 
   params = curve_fit(lambda Vbe, β: np.log(ib_f(Vbe, β)), thisVb, np.log(thisIb))
   β = params[0][0]
@@ -59,11 +65,8 @@ ax = plt.subplot(111)
 for i in range(4):
   ax.semilogy(vb_exp[i], ib_exp[i], ['r.', 'y.', 'g.', 'b.'][i], label="Base current (%i)" % (i+1))
   ax.semilogy(vb_exp[i], ic_exp[i], ['ro', 'yo', 'go', 'bo'][i], label="Collector current (%i)" % (i+1), markersize=1)
-  ax.semilogy(vb_exp[i], ic_f(vb_exp[i], Uts[i], Iss[i]), ['r-', 'y-', 'g-', 'b-'][i], label="Theoretical Ic (Ut = %.4g, Is = %.4g)" % (Uts[i], Iss[i]))
-  ax.semilogy(vb_exp[i], ib_f(vb_exp[i], βs[i]), ['r--', 'y--', 'g--', 'b--'][i], label="Theoretical Ib (Ut = %.4g, Is = %.4g, β = %.4g)" % (Uts[i], Iss[i], βs[i]))
-
-# ax.axvline(vb_exp[valid[0]], label='Sampled Data')
-# ax.axvline(vb_exp[valid[1]])
+  ax.semilogy(vb_exp[i], ic_f(vb_exp[i], Uts[i], Iss[i]), ['r-', 'y-', 'g-', 'b-'][i], label="Theoretical Ic (%i)" % (i+1))
+  ax.semilogy(vb_exp[i], ib_f(vb_exp[i], βs[i]), ['r--', 'y--', 'g--', 'b--'][i], label="Theoretical Ib (%i)" % (i+1))
 
 plt.title("Transistor Currents vs Base Voltages")
 plt.xlabel("Base Voltage (V)")
