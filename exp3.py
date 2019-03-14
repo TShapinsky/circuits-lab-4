66c852efe7199b4721b3da2833f5e7e2b9de9246
Adam Selker <adamselker@gmail.com>
2019-03-13 22:03:28 -0400

    fix ranges in exp3

--- exp3.py
+++ exp3.py
@@ -37,13 +37,21 @@ def iz_f(ix, iy): return np.power(ix, 2)/iy
 iz1t = [[iz_f(ix, iy) for ix in ixs] for (ixs, iy) in zip(ix1, iy1)]
 iz2t = [[iz_f(ix, iy) for iy in iys] for (ix, iys) in zip(ix2, iy2)]
 
+def clip_range(xs, ys, bounds):
+  pairs = [(x, y) for (x, y) in zip(xs, ys) if (bounds[0] <= y) and (y <= bounds[1])]
+  return list(zip(*pairs))
+
+
+clipped1 = [clip_range(ix, iz, (1e-8, 1e-1)) for (ix, iz) in zip(ix1, iz1t)]
+clipped2 = [clip_range(iy, iz, (1e-9, 1e-2)) for (iy, iz) in zip(iy2, iz2t)]
+
 fig = plt.figure(figsize=(8,6))
 ax = plt.subplot(111)
 
 # Sink log-log plot
 for i in range(3):
   ax.loglog(ix1[i], iz1[i], ['r.', 'g.', 'b.'][i], label="Iz (Iy = %s)" % iy_names[i])
-  ax.loglog(ix1[i], iz1t[i], ['r-', 'g-', 'b-'][i], label="Theoretical fit (Iy = %s)" % iy_names[i])
+  ax.loglog(clipped1[i][0], clipped1[i][1], ['r-', 'g-', 'b-'][i], label="Theoretical fit (Iy = %s)" % iy_names[i])
 
 plt.title("Quadratic inverter (fixed Iy)")
 plt.xlabel("Input current Ix (A)")
@@ -56,7 +64,7 @@ ax.cla()
 # Source log-log plot
 for i in range(3):
   ax.loglog(iy2[i], iz2[i], ['r.', 'g.', 'b.'][i], label="Iz (Ix = %s)" % ix_names[i])
-  ax.loglog(iy2[i], iz2t[i], ['r-', 'g-', 'b-'][i], label="Theoretical fit (Ix = %s)" % ix_names[i])
+  ax.loglog(clipped2[i][0], clipped2[i][1], ['r-', 'g-', 'b-'][i], label="Theoretical fit (Ix = %s)" % ix_names[i])
 
 plt.title("Quadratic inverter (fixed Ix)")
 plt.xlabel("Input current Iy (A)")
